3a50330b11242f0b80432297f7a68c3694a7991b
joaoserafinadm <joaoserafin.adm@gmail.com>
2024-10-17 22:54:39 -0300

    feat: teste assinatura produção

--- pages/api/accountSetup/subscription.js
+++ pages/api/accountSetup/subscription.js
@@ -37,8 +37,8 @@ export default authenticated(async (req, res) => {
         }
 
         try {
-            const startDate = new Date();
-            startDate.setMinutes(startDate.getMinutes() + 5); // Ajuste do start_date para evitar problemas de fuso horário
+            // const startDate = new Date();
+            // startDate.setMinutes(startDate.getMinutes() + 5); // Ajuste do start_date para evitar problemas de fuso horário
 
             const subscriptionResponse = await fetch('https://api.mercadopago.com/preapproval', {
                 method: 'POST',
@@ -56,7 +56,7 @@ export default authenticated(async (req, res) => {
                         frequency_type: 'months', // Frequência mensal
                         transaction_amount: 79.90,
                         currency_id: "BRL", // Moeda
-                        start_date: startDate.toISOString(), // Incluindo a data de início para evitar o problema de fuso horário
+                        // start_date: startDate.toISOString(), // Incluindo a data de início para evitar o problema de fuso horário
                     },
                     status: "authorized", // Definindo o status como autorizado
                     back_url: 'https://app.avaliaimobi.com.br', // URL de redirecionamento após a assinatura (opcional)
