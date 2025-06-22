86bcb78b05bb957b74ccdf11cd7ca6ca22101aee
joaoserafinadm <joaoserafin.adm@gmail.com>
2025-06-22 19:07:45 -0300

    feat: menubar

--- src/components/menuBar/index.jsx
+++ src/components/menuBar/index.jsx
@@ -8,19 +8,13 @@ import { useDispatch } from 'react-redux'
 import { toggleBarChange } from '../../../store/ToggleBarStatus/ToggleBarStatus.action'
 import isMobile from '../../../utils/isMobile'
 
-
 export default function MenuBar(props) {
-
     const dispatch = useDispatch()
-
     const router = useRouter()
-
     const [pathname, setPathname] = useState('')
 
     useEffect(() => {
-
         setPathname(router.pathname)
-
     }, [router.pathname])
 
     const permitedPages = [
@@ -28,98 +22,64 @@ export default function MenuBar(props) {
         '/clientsManagement',
         '/tutorials',
         '/sac',
-        // '/passwordChange',
         '/accountSetup',
         '/'
     ]
 
-
-
-
+    const menuItems = [
+        {
+            href: '/',
+            icon: faHome,
+            label: 'Início',
+            path: '/'
+        },
+        {
+            href: '/usersManagement',
+            icon: faUsers,
+            label: 'Usuários',
+            path: '/usersManagement'
+        },
+        {
+            href: '/clientsManagement',
+            icon: faHomeUser,
+            label: 'Clientes',
+            path: '/clientsManagement'
+        },
+        {
+            href: null,
+            icon: faList,
+            label: 'Opções',
+            path: '/accountSetup',
+            onClick: () => dispatch(toggleBarChange(false))
+        }
+    ]
 
     return (
-        <div className={` ${permitedPages.includes(pathname) && isMobile() ? styles.container : styles.containerHide}`}>
-            <div className="row h-100 px-4">
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-
-                    <Link href='/'
-                        className={`text-center  ${pathname === '/' ? `${styles.pageSelected}` : 'text-light'}`}>
-                        <FontAwesomeIcon icon={faHome} /> <br />
-                        <span style={{ fontSize: '10px' }}>Início</span>
-                    </Link>
-
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-
-                    <Link href='/usersManagement'
-                        className={`text-center  ${pathname === '/usersManagement' ? `${styles.pageSelected}` : 'text-light'}`}>
-                        <FontAwesomeIcon icon={faUsers} /> <br />
-                        <span style={{ fontSize: '10px' }}>Usuários</span>
-                    </Link>
-
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-
-                    <Link href='/clientsManagement'
-                        className={`text-center  ${pathname === '/clientsManagement' ? `${styles.pageSelected}` : 'text-light'}`}>
-                        <FontAwesomeIcon icon={faHomeUser} /> <br />
-                        <span style={{ fontSize: '10px' }}>Clientes</span>
-                    </Link>
-
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center px-0">
-
-                    <span onClick={() =>dispatch(toggleBarChange(false))}
-                        className={`text-center  ${pathname === '/accountSetup' ? `${styles.pageSelected}` : 'text-light'}`}>
-                        <FontAwesomeIcon icon={faList} /> <br />
-                        <span style={{ fontSize: '10px' }}>Opções</span>
-                    </span>
-
-                </div>
+        <div className={`${permitedPages.includes(pathname) && isMobile() ? styles.container : styles.containerHide}`}>
+            <div className={styles.menuWrapper}>
+                {menuItems.map((item, index) => {
+                    const isActive = pathname === item.path
+                    const Component = item.href ? Link : 'button'
+                    
+                    return (
+                        <div key={index} className={styles.menuItem}>
+                            <Component
+                                {...(item.href ? { href: item.href } : { onClick: item.onClick })}
+                                className={`${styles.menuLink} ${isActive ? styles.active : ''}`}
+                            >
+                                <div className={styles.iconWrapper}>
+                                    <FontAwesomeIcon 
+                                        icon={item.icon} 
+                                        className={styles.icon}
+                                    />
+                                </div>
+                                <span className={styles.label}>{item.label}</span>
+                                {isActive && <div className={styles.ripple} />}
+                            </Component>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
 }
-
-
-
-{/* <div className="col-3 d-flex justify-content-center align-items-center ">
-                    <span>
-                        <FontAwesomeIcon icon={faUsers} />
-                    </span>
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-                    <span>
-                        <FontAwesomeIcon icon={faUsers} />
-                    </span>
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-                    <span>
-                        <FontAwesomeIcon icon={faUsers} />
-                    </span>
-                </div>
-                <div className="col-3 d-flex justify-content-center align-items-center ">
-                    <span>
-                        <FontAwesomeIcon icon={faUsers} />
-                    </span>
-                </div> */}
-
-
-
-
-
-// <div>
-
-//         <Link href='/usersManagement' className='text-center text-light '>
-//             <FontAwesomeIcon icon={faUsers} /> <br />
-//             <span className='small'>Usuários</span>
-//         </Link>
-//     </div>
-//     <Link href='/clientsManagement' className='text-center text-light '>
-//         <FontAwesomeIcon icon={faHomeUser} /> <br />
-//         <span className='small'>Clientes</span>
-//     </Link>
-//     <Link href='/clientsManagement' className='text-center text-light '>
-//         <FontAwesomeIcon icon={faList} /> <br />
-//         <span className='small'>Imobiliária</span>
-//     </Link>
