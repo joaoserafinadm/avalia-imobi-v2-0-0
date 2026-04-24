2f5a0c6914b0a9414f65af5221c7f890d64cb868
joaoserafinadm <joaoserafin.adm@gmail.com>
2026-04-23 23:23:28 -0300

    feat: locations google api

--- src/pages/newClient/Location.jsx
+++ src/pages/newClient/Location.jsx
@@ -1,57 +1,101 @@
 import Map from "./Map"
+import React, { useRef, useEffect, useState } from "react"
+import { useJsApiLoader } from "@react-google-maps/api"
 import { useDispatch, useSelector } from "react-redux"
 import { setBairro, setCep, setCidade, setLatitude, setLogradouro, setLongitude, setNumero, setUf } from "../../../store/NewClientForm/NewClientForm.actions"
-import buscaCep from "../../../utils/buscaCep"
-import { maskCep } from "../../../utils/mask"
 import TitleLabel from "../../components/TitleLabel"
-import Input from "../../components/Input"
 import s from "./formInputs.module.scss"
 
-const UF_OPTIONS = [
-    { value: 'AC', label: 'AC' }, { value: 'AL', label: 'AL' }, { value: 'AM', label: 'AM' },
-    { value: 'AP', label: 'AP' }, { value: 'BA', label: 'BA' }, { value: 'CE', label: 'CE' },
-    { value: 'DF', label: 'DF' }, { value: 'ES', label: 'ES' }, { value: 'GO', label: 'GO' },
-    { value: 'MA', label: 'MA' }, { value: 'MG', label: 'MG' }, { value: 'MS', label: 'MS' },
-    { value: 'MT', label: 'MT' }, { value: 'PA', label: 'PA' }, { value: 'PB', label: 'PB' },
-    { value: 'PE', label: 'PE' }, { value: 'PI', label: 'PI' }, { value: 'PR', label: 'PR' },
-    { value: 'RJ', label: 'RJ' }, { value: 'RN', label: 'RN' }, { value: 'RO', label: 'RO' },
-    { value: 'RR', label: 'RR' }, { value: 'RS', label: 'RS' }, { value: 'SC', label: 'SC' },
-    { value: 'SE', label: 'SE' }, { value: 'SP', label: 'SP' }, { value: 'TO', label: 'TO' },
-]
+const LIBRARIES = ["places"]
 
 export default function Location(props) {
     const newClientForm = useSelector(state => state.newClientForm)
     const dispatch = useDispatch()
 
-    const onBlurCep = async (event) => {
-        const adress = await buscaCep(event)
-        if (adress) {
-            dispatch(setLogradouro(adress.logradouro))
-            dispatch(setBairro(adress.bairro))
-            dispatch(setCidade(adress.localidade))
-            dispatch(setUf(adress.uf))
-        }
+    const { isLoaded } = useJsApiLoader({
+        id: "avalia-imobi",
+        googleMapsApiKey: "AIzaSyAU54iwv20-0BDGcVzMcMrVZpmZRPJDDic",
+        libraries: LIBRARIES,
+    })
+
+    const autocompleteRef = useRef(null)
+    const inputRef = useRef(null)
+
+    const buildAddressDisplay = () => {
+        const parts = [
+            newClientForm.logradouro,
+            newClientForm.numero,
+            newClientForm.bairro,
+            newClientForm.cidade,
+            newClientForm.uf,
+        ].filter(Boolean)
+        return parts.join(", ")
     }
 
-    const getCoordinates = async () => {
-        if (newClientForm.cidade && newClientForm.uf && newClientForm.logradouro) {
-            try {
-                const address = `${newClientForm.logradouro} ${newClientForm.numero}, ${newClientForm.bairro}, ${newClientForm.cidade}, ${newClientForm.uf}`
-                if (newClientForm.numero) {
-                    const response = await fetch(
-                        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyAU54iwv20-0BDGcVzMcMrVZpmZRPJDDic`
-                    )
-                    if (response.ok) {
-                        const data = await response.json()
-                        const location = data.results[0].geometry.location
-                        dispatch(setLatitude(location.lat))
-                        dispatch(setLongitude(location.lng))
-                    }
-                }
-            } catch (error) {
-                console.error('Erro ao obter coordenadas', error)
+    const [addressInput, setAddressInput] = useState("")
+
+    useEffect(() => {
+        const built = buildAddressDisplay()
+        if (built) setAddressInput(built)
+    }, [
+        newClientForm.logradouro,
+        newClientForm.bairro,
+        newClientForm.cidade,
+        newClientForm.uf,
+        newClientForm.numero,
+    ])
+
+    useEffect(() => {
+        if (!isLoaded || !inputRef.current) return
+
+        autocompleteRef.current = new window.google.maps.places.Autocomplete(
+            inputRef.current,
+            {
+                types: ["geocode"],
+                componentRestrictions: { country: "br" },
+                fields: ["address_components", "geometry", "formatted_address"],
             }
+        )
+
+        autocompleteRef.current.addListener("place_changed", handlePlaceSelect)
+    }, [isLoaded])
+
+    const handlePlaceSelect = () => {
+        const place = autocompleteRef.current?.getPlace()
+        if (!place?.geometry) return
+
+        const components = place.address_components || []
+
+        const get = (types) => {
+            const c = components.find(comp => types.every(t => comp.types.includes(t)))
+            return c?.long_name || ""
         }
+
+        const getShort = (types) => {
+            const c = components.find(comp => types.every(t => comp.types.includes(t)))
+            return c?.short_name || ""
+        }
+
+        const streetNumber = get(["street_number"])
+        const route = get(["route"])
+        const sublocality = get(["sublocality_level_1"]) || get(["sublocality"]) || get(["neighborhood"])
+        const city = get(["locality"]) || get(["administrative_area_level_2"])
+        const uf = getShort(["administrative_area_level_1"])
+        const postalCode = get(["postal_code"]).replace("-", "")
+
+        const lat = place.geometry.location.lat()
+        const lng = place.geometry.location.lng()
+
+        dispatch(setLogradouro(route))
+        dispatch(setNumero(streetNumber))
+        dispatch(setBairro(sublocality))
+        dispatch(setCidade(city))
+        dispatch(setUf(uf))
+        if (postalCode) dispatch(setCep(postalCode))
+        dispatch(setLatitude(lat))
+        dispatch(setLongitude(lng))
+
+        setAddressInput(place.formatted_address || "")
     }
 
     return (
@@ -60,48 +104,20 @@ export default function Location(props) {
             <div className={s.section}>
                 <div className="row g-3">
 
-                    <div className="col-12 col-md-3 col-xl-2">
-                        <Input type="text" label="CEP" required
-                            value={newClientForm.cep}
-                            onChange={e => dispatch(setCep(maskCep(e.target.value)))}
-                            onBlur={e => { onBlurCep(e); getCoordinates() }} />
-                    </div>
-
-                    <div className="col-12 col-md-7 col-xl-4">
-                        <Input type="text" label="Logradouro"
-                            value={newClientForm.logradouro}
-                            onChange={e => dispatch(setLogradouro(e.target.value))}
-                            onBlur={() => getCoordinates()} />
-                    </div>
-
-                    <div className="col-12 col-md-2 col-xl-1">
-                        <Input type="text" label="Número"
-                            value={newClientForm.numero}
-                            onChange={e => dispatch(setNumero(e.target.value))}
-                            onBlur={() => getCoordinates()} />
-                    </div>
-
-                    <div className="col-12 col-md-5 col-xl-2">
-                        <Input type="text" label="Bairro" required
-                            value={newClientForm.bairro}
-                            onChange={e => dispatch(setBairro(e.target.value))}
-                            onBlur={() => getCoordinates()} />
-                    </div>
-
-                    <div className="col-12 col-md-5 col-xl-2">
-                        <Input type="text" label="Cidade" required
-                            value={newClientForm.cidade}
-                            onChange={e => dispatch(setCidade(e.target.value))}
-                            onBlur={() => getCoordinates()} />
-                    </div>
-
-                    <div className="col-12 col-md-2 col-xl-1">
-                        <Input type="select" label="UF" required
-                            placeholder="UF"
-                            value={newClientForm.uf}
-                            onChange={e => dispatch(setUf(e.target.value))}
-                            onBlur={() => getCoordinates()}
-                            options={UF_OPTIONS} />
+                    <div className="col-12">
+                        <label className="form-label">Endereço<b>*</b></label>
+                        <input
+                            ref={inputRef}
+                            type="text"
+                            className="form-control"
+                            placeholder="Digite o endereço do imóvel..."
+                            value={addressInput}
+                            onChange={e => setAddressInput(e.target.value)}
+                            autoComplete="off"
+                        />
+                        <small className="text-muted">
+                            Digite e selecione uma sugestão para preencher automaticamente
+                        </small>
                     </div>
 
                     {newClientForm.latitude && newClientForm.longitude && (
--- src/valuation/LocationValuation.jsx
+++ src/valuation/LocationValuation.jsx
@@ -1,11 +1,6 @@
 import Map from "./Map";
-import React, { useRef, useEffect, useState, useCallback } from "react";
-import {
-  GoogleMap,
-  Marker,
-  useJsApiLoader,
-  Autocomplete,
-} from "@react-google-maps/api";
+import React, { useRef, useEffect, useState } from "react";
+import { useJsApiLoader } from "@react-google-maps/api";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setBairro,
@@ -17,149 +12,187 @@ import {
   setNumero,
   setUf,
 } from "../../store/NewClientForm/NewClientForm.actions";
-import buscaCep from "../../utils/buscaCep";
-import { maskCep } from "../../utils/mask";
-import EstadosList from "../components/estadosList";
 import { SpinnerSM } from "../components/loading/Spinners";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocation, faLocationDot, faMapLocation, faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
-const libraries = ["places"];
+const LIBRARIES = ["places"];
 
 export default function LocationValuation(props) {
   const newClientForm = useSelector((state) => state.newClientForm);
   const dispatch = useDispatch();
 
-  const [zoom, setZoom] = useState(16);
+  const { isLoaded } = useJsApiLoader({
+    id: "avalia-imobi",
+    googleMapsApiKey: "AIzaSyAU54iwv20-0BDGcVzMcMrVZpmZRPJDDic",
+    libraries: LIBRARIES,
+  });
 
+  const autocompleteRef = useRef(null);
+  const inputRef = useRef(null);
   const [loadingMap, setLoadingMap] = useState(false);
 
-  // useEffect(() => {
+  // Monta o endereço legível a partir dos campos separados (para imóveis já cadastrados)
+  const buildAddressDisplay = () => {
+    const parts = [
+      newClientForm.logradouro,
+      newClientForm.numero,
+      newClientForm.bairro,
+      newClientForm.cidade,
+      newClientForm.uf,
+    ].filter(Boolean);
+    return parts.join(", ");
+  };
 
-  //     console.log("newClientForm", newClientForm.bairro)
-  //     if ( newClientForm.cidade && newClientForm.uf) {
-  //         getCoordinates()
-  //     }
+  const [addressInput, setAddressInput] = useState("");
 
-  // }, [newClientForm.logradouro, newClientForm.bairro, newClientForm.cidade, newClientForm.uf])
+  // Sincroniza o input com dados vindos do store (edição de imóvel existente)
+  useEffect(() => {
+    const built = buildAddressDisplay();
+    if (built) setAddressInput(built);
+  }, [
+    newClientForm.logradouro,
+    newClientForm.bairro,
+    newClientForm.cidade,
+    newClientForm.uf,
+    newClientForm.numero,
+  ]);
 
+  // Inicializa o Google Autocomplete assim que a API estiver carregada
+  useEffect(() => {
+    if (!isLoaded || !inputRef.current) return;
+
+    autocompleteRef.current = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      {
+        types: ["geocode"],
+        componentRestrictions: { country: "br" },
+        fields: ["address_components", "geometry", "formatted_address"],
+      }
+    );
+
+    autocompleteRef.current.addListener("place_changed", handlePlaceSelect);
+  }, [isLoaded]);
+
+  const handlePlaceSelect = () => {
+    const place = autocompleteRef.current?.getPlace();
+    if (!place?.geometry) return;
+
+    const components = place.address_components || [];
+
+    const get = (types) => {
+      const c = components.find((comp) =>
+        types.every((t) => comp.types.includes(t))
+      );
+      return c?.long_name || "";
+    };
+
+    const getShort = (types) => {
+      const c = components.find((comp) =>
+        types.every((t) => comp.types.includes(t))
+      );
+      return c?.short_name || "";
+    };
+
+    const streetNumber = get(["street_number"]);
+    const route = get(["route"]);
+    const sublocality =
+      get(["sublocality_level_1"]) ||
+      get(["sublocality"]) ||
+      get(["neighborhood"]);
+    const city =
+      get(["locality"]) ||
+      get(["administrative_area_level_2"]);
+    const uf = getShort(["administrative_area_level_1"]);
+    const postalCode = get(["postal_code"]).replace("-", "");
+
+    const lat = place.geometry.location.lat();
+    const lng = place.geometry.location.lng();
+
+    dispatch(setLogradouro(route));
+    dispatch(setNumero(streetNumber));
+    dispatch(setBairro(sublocality));
+    dispatch(setCidade(city));
+    dispatch(setUf(uf));
+    if (postalCode) dispatch(setCep(postalCode));
+    dispatch(setLatitude(lat));
+    dispatch(setLongitude(lng));
+
+    setAddressInput(place.formatted_address || "");
+  };
+
+  // Fallback: geocodifica o endereço manualmente caso o usuário não selecione sugestão
   const getCoordinates = async () => {
     setLoadingMap(true);
-
     if (!newClientForm.cidade || !newClientForm.uf) {
       setLoadingMap(false);
       return;
     }
     try {
-      // Construa o endereço a partir das partes disponíveis (logradouro, número, bairro, cidade, uf)
-      const address = `${newClientForm.logradouro}, ${newClientForm.bairro}, ${newClientForm.cidade}, ${newClientForm.uf}`;
+      const address = [
+        newClientForm.logradouro,
+        newClientForm.numero,
+        newClientForm.bairro,
+        newClientForm.cidade,
+        newClientForm.uf,
+      ]
+        .filter(Boolean)
+        .join(", ");
 
-      // Execute a geocodificação usando a API de Geocodificação do Google Maps
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
           address
         )}&key=AIzaSyAU54iwv20-0BDGcVzMcMrVZpmZRPJDDic`
       );
 
-      // Verifique se a resposta da API é bem-sucedida
       if (response.ok) {
         const data = await response.json();
-
-        // Extraia as coordenadas da resposta
-        const location = data.results[0].geometry.location;
-
-        dispatch(setLatitude(location.lat));
-        dispatch(setLongitude(location.lng));
-
-        setZoom(16);
-        setLoadingMap(false);
-      } else {
-        setLoadingMap(false);
-
-        console.error("Erro ao obter coordenadas");
+        const location = data.results[0]?.geometry?.location;
+        if (location) {
+          dispatch(setLatitude(location.lat));
+          dispatch(setLongitude(location.lng));
+        }
       }
     } catch (error) {
+      // silêncio intencional — erro de geocodificação não bloqueia o formulário
+    } finally {
       setLoadingMap(false);
-
-      console.error("Erro ao obter coordenadas", error);
     }
-    setLoadingMap(false);
   };
 
   return (
     <div className="row fadeItem mt-3">
-      <label htmlFor="geralForm" className="form-label fw-bold">
-        Localização
-      </label>
+      <label className="form-label fw-bold">Localização</label>
       <div className="row">
-        <div className="col-12 col-sm-6 col-lg-4 my-2  pe-1">
-          <label for="geralForm" className="form-label">
-            Logradouro
+        {/* Busca de endereço via Google Autocomplete */}
+        <div className="col-12 col-lg-9 my-2 pe-1">
+          <label className="form-label">
+            Endereço<b>*</b>
           </label>
           <input
+            ref={inputRef}
             type="text"
             className="form-control"
-            name="clientLastNameItem"
-            id="clientLastNameItem"
-            onBlur={() => getCoordinates()}
-            value={newClientForm.logradouro}
-            onChange={(e) => dispatch(setLogradouro(e.target.value))}
+            placeholder="Digite o endereço do imóvel..."
+            value={addressInput}
+            onChange={(e) => setAddressInput(e.target.value)}
+            autoComplete="off"
           />
+          <small className="text-muted">
+            Digite e selecione uma sugestão para preencher automaticamente
+          </small>
         </div>
 
-        <div className="col-12 col-sm-6  col-lg-3 my-2  pe-1">
-          <label for="geralForm" className="form-label">
-            Bairro<b>*</b>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            name="celularItem"
-            id="celularItem"
-            onBlur={() => getCoordinates()}
-            value={newClientForm.bairro}
-            onChange={(e) => dispatch(setBairro(e.target.value))}
-          />
-        </div>
-        <div className="col-12 col-sm-6 col-lg-3 my-2  pe-1">
-          <label for="geralForm" className="form-label">
-            Cidade<b>*</b>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            name="celularItem"
-            id="celularItem"
-            onBlur={() => getCoordinates()}
-            value={newClientForm.cidade}
-            onChange={(e) => dispatch(setCidade(e.target.value))}
-          />
-        </div>
-
-        <div className="col-12 col-sm-6 col-lg-2 my-2  pe-1">
-          <label for="geralForm" className="form-label">
-            UF<b>*</b>
-          </label>
-
-          <select
-            className="form-select"
-            placeholder="Estado"
-            value={newClientForm.uf}
-            onChange={(e) => dispatch(setUf(e.target.value))}
-            onBlur={() => getCoordinates()}
-          >
-            <EstadosList />
-          </select>
-        </div>
-        <div className="col-12 d-flex justify-content-end">
+        {/* <div className="col-12 d-flex justify-content-end">
           <button
             type="button"
             className="btn btn-outline-orange"
-            onClick={() => getCoordinates()}
+            onClick={getCoordinates}
           >
-            <FontAwesomeIcon icon={faLocationDot} className="me-2"/>Buscar coordenadas
+            <FontAwesomeIcon icon={faLocationDot} className="me-2" />
+            Buscar coordenadas
           </button>
-        </div>
+        </div> */}
 
         <div className="col-12 my-2 mb-4">
           {loadingMap ? (
