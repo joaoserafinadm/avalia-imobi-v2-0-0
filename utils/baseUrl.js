486f52fe9ac3793aa5d6412bab7b8d1f0ced532e
joaoserafinadm <joaoserafin.adm@gmail.com>
2026-05-05 00:01:47 -0300

    feat: v2 em produção

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -4,10 +4,10 @@ export default function baseUrl() {
     // const baseUrl = 'http://localhost:3000'
 
     // Teste
-    const baseUrl = 'https://avalia-imobi-v2-0-0.vercel.app'
+    // const baseUrl = 'https://avalia-imobi-v2-0-0.vercel.app'
 
     //Produção
-    // const baseUrl = 'https://app.avaliaimobi.com.br'
+    const baseUrl = 'https://app.avaliaimobi.com.br'
     
 
     return baseUrl
